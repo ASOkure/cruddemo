f9677d14519daa36bfb7dcc786cff0addae3b904
Akanimo Sam Okure <akan.okure@gmail.com>
2024-04-19 11:21:56 +0100

    add and implement employee entity

--- src/main/java/com/aso/cruddemopro/entity/Employee.java
+++ src/main/java/com/aso/cruddemopro/entity/Employee.java
@@ -1,11 +1,75 @@
 package com.aso.cruddemopro.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name="employee")
 public class Employee {
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id")
+    private  int id;
+
+  @Column(name="firstName")
+    private String firstName;
+  @Column(name="lastName")
+  private String lastName;
+  @Column(name="email")
+  private String email;
+
+    // define constructors
+
+public  Employee(){
+
+}
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
