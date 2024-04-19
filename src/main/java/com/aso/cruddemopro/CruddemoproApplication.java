ee2155eb95143d47e195d560a7f81ed720d73758
Akanimo Sam Okure <akan.okure@gmail.com>
2024-04-19 10:33:05 +0100

    initial set up

---
+++ src/main/java/com/aso/cruddemopro/CruddemoproApplication.java
@@ -0,0 +1,13 @@
+package com.aso.cruddemopro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aso/cruddemopro/CruddemoproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aso.cruddemopro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
