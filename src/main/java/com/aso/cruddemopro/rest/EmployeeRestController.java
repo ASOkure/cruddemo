b18215959132f4376f8179b6b21a07e71cad780b
Akanimo Sam Okure <akan.okure@gmail.com>
2024-04-19 23:09:31 +0100

    implement crud operations in rest controller

--- src/main/java/com/aso/cruddemopro/rest/EmployeeRestController.java
+++ src/main/java/com/aso/cruddemopro/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ package com.aso.cruddemopro.rest;
 import com.aso.cruddemopro.dao.EmployeeDAO;
 import com.aso.cruddemopro.entity.Employee;
 import com.aso.cruddemopro.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,4 +29,64 @@ public class EmployeeRestController {
 
         return employeeService.findAll();
     }
-}
+
+    @GetMapping("/employees/{employeeId}")
+
+    public Employee getEmployee(@PathVariable  int employeeId){
+
+        Employee theEmployee =  employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+
+            throw new RuntimeException(" Employee id not found - " + employeeId);
+        }
+
+                return theEmployee;
+
+        }
+
+        // add mapping for POST /employees - add new employee
+
+     @PostMapping("/employees")
+
+    public  Employee addEmployee(@RequestBody Employee theEmployee){
+
+        theEmployee.setId(0);
+
+      Employee addEmployee =  employeeService.save(theEmployee);
+
+       return  addEmployee;
+     }
+
+     // add mapping for PUT /employees- update existing employee
+
+    @PutMapping("/employees")
+
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+
+        Employee dbemployee = employeeService.save(theEmployee);
+
+        return dbemployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeid} - delete employee
+
+    @DeleteMapping("/employees/{employeeId}")
+
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee temEmployee = employeeService.findById(employeeId);
+
+        if (temEmployee == null) {
+
+            throw new RuntimeException(" Employeee does not exist " + employeeId);
+        }
+             employeeService.deleteById(employeeId);
+
+        return " Deleted employee id - " + employeeId;
+        }
+
+
+    }
+
+
