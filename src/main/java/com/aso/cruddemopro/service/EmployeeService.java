b49f673f8751a3760167d7108ea9cb6d6f5b51cb
Akanimo Sam Okure <akan.okure@gmail.com>
2024-04-19 13:25:53 +0100

    add and implement employee service

---
+++ src/main/java/com/aso/cruddemopro/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.aso.cruddemopro.service;
+
+import com.aso.cruddemopro.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+}
